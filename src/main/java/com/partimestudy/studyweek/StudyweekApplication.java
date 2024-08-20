6951f14587489c8d88986d81c60c1c96cdaab1c1
helloJosh <kimjoshua135@gmail.com>
2024-08-20 16:48:27 +0900

    feat: first commit

---
+++ src/main/java/com/partimestudy/studyweek/StudyweekApplication.java
@@ -0,0 +1,13 @@
+package com.partimestudy.studyweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyweekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyweekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/partimestudy/studyweek/StudyweekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partimestudy.studyweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyweekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
